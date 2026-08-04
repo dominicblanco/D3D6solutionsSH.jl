0aa5191648377b3512c389c58e03ee02c1a85afd
dominicblanco <119467196+dominicblanco@users.noreply.github.com>
2026-08-04 07:44:12 -0400

    Update D6SHPeriodic.jl

--- D6SHPeriodic.jl
+++ D6SHPeriodic.jl
@@ -49,39 +49,85 @@ function CAP(Y₀,Z₁,Z₂)
     end
 end
 
+function f(u,μ,γ)
+    f1 = similar(u)
+    Δ = project(Laplacian(2),space(u),space(u),Float64)
+    project!(f1,-(I + Δ)^2*u - μ*u + γ*u^2 - u^3)
+    return f1
+end
+
+function Df(u,μ,γ)
+    Df1 = LinearOperator(space(u),space(u),zeros(dimension(space(u)),dimension(space(u))))
+    Δ = project(Laplacian(2),space(u),space(u),Float64)
+    U = project(Multiplication(u),space(u),space(u),Float64)
+    U² = project(Multiplication(u^2),space(u),space(u),Float64)
+    Df1 = -(I + Δ)^2 - μ*I + 2γ * U - 3* U²
+    return Df1
+end
+
+function _newton(a,μ,γ)
+    ϵ = 1
+    nv = 1
+    j = 0
+    while (ϵ > 1e-14) & (j < 15)
+        F = f(a,μ,γ)
+        DF = Df(a,μ,γ)
+        a = a - DF\F
+        @show ϵ = norm(F,Inf)
+        if ϵ > 7
+            display("Newton may have diverged")
+            CrashNow = Badk
+            return a,ϵ
+        end
+        nv = norm(a)
+        if nv < 1e-5
+            @show nv = norm(a)
+            display("Newton may have converged to the 0 solution")
+            return nv,j
+            break
+        end
+        j += 1
+    end
+    return a,ϵ
+end
+
 ################### PROOF OF D₆ SOLUTION : MAIN CODE #################################################################################################################################################
 #Solution 1
 ū = load("ubar_Th_3_9","ubar")
-N = 30
+N = 80
 d = interval(10)
 μ = interval(-0.01)
 γ = interval(1.6)
-r₀ = interval(6e-3)
-ν = interval(1.3)
+r₀ = interval(3e-8)
+ν = interval(1.09)
+ū, err = _newton(project(ū,D₆Fourier(N,π/mid(d))),mid(μ),mid(γ))
 #=Solution 2
 ū = load("ubar_Th_3_10","ubar")
-N = 26
+N = 80
 d = interval(10)
 μ = interval(-0.1)
 γ = interval(2)
-r₀ = interval(3e-5)
-ν = interval(1.37)=#
+r₀ = interval(3e-8)
+ν = interval(1.09)
+ū, err = _newton(project(ū,D₆Fourier(N,π/mid(d))),mid(μ),mid(γ))=#
 #=Solution 3
 ū = load("ubar_Th_3_11","ubar")
-N = 16
+N = 80
 d = interval(5)
 μ = interval(0.3)
 γ = interval(2.1)
-r₀ = interval(7e-5)
-ν = interval(1.4)=#
+r₀ = interval(3e-8)
+ν = interval(1.09)
+ū, err = _newton(project(ū,D₆Fourier(N,π/mid(d))),mid(μ),mid(γ))=#
 #=Solution 4
-N = 56
+N = 80
 d = interval(15)
 ū = load("ubar_Th_3_12","ubar")
 μ = interval(0.25)
 γ = interval(2)
-r₀ = interval(4e-5)
-ν = interval(1.09)=#
+r₀ = interval(3e-8)
+ν = interval(1.09)
+ū, err = _newton(project(ū,D₆Fourier(N,frequency(ū)[1])),mid(μ),mid(γ))=#
 
 fourier = D₆Fourier(N,π/d)
 ū_interval = Sequence(fourier, interval.(coefficients(ū)))
@@ -116,10 +162,7 @@ Y₀ = norm(A*project(L_diag.*ū_interval+G,fourier),X) + interval(1)/L_N*norm(
 ################################ Z₂ BOUND ######################################################
 # Computation of the Z₂ bound defined in Lemma 3.3.
 q = interval(2)*γ - interval(6)*ū_interval
-#=𝕢 = project(Multiplication(q),D₆Fourier(2N,π/d),fourier,Interval{Float64})
-Z₂ = interval(3)*(norm_A + interval(1)/L_N)*r₀ + opnorm(LinearOperator(coefficients(P.*(A*𝕢).*P⁻¹2')),1) + interval(1)/L_N*norm(q,X)
-@show Z₂=#
-Z₂ = (norm_A + interval(1)/L_N)*(norm(q,X) + r₀)
+Z₂ = max(norm_A,interval(1)/L_N)*(norm(q,X) + r₀)
 @show Z₂
 ################################ Z₀ BOUND ######################################################
 # Computation of the Z₀ bound defined in Lemma 3.2.
